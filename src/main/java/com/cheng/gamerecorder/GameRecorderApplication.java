cba0af11ffc4821c8facf4d698b6b707a91443bd
ChengLu <2854859592@qq.com>
2023-12-06 16:37:35 +0800

    init

---
+++ src/main/java/com/cheng/gamerecorder/GameRecorderApplication.java
@@ -0,0 +1,13 @@
+package com.cheng.gamerecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRecorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheng/gamerecorder/GameRecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheng.gamerecorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
